59cb4bfc2bfbf6bf0d9fd490d9c2f0f5f48cb43b
Anirudh Duggal <anirudhdggl@gmail.com>
2020-07-26 15:53:14 +0530

    added the update clipboard function

--- main.py
+++ main.py
@@ -14,3 +14,18 @@ def copyToClipboard():
   txt = pc.paste()
   if txt not in clipboard:
     clipboard.insert(0, txt)
+
+def updateClip(frame):
+  widgets = frame.winfo_children()
+
+  copyToClipboard()
+
+  diff = len(clipboard) - len(widgets)
+
+  for i in range(diff):
+    label = Label(frame,padx=5,pady=10,background="white",text=clipboard[i],relief=RAISED, anchor="w",)
+    label.bind("<Button-1>", lambda e, txt = label["text"]: copyFromClipboard(txt))
+    label.pack(fill=X)
+    frame.pack()
+    
+  root.after(500, updateClip, frame)
